7787e06b4752aaa780525ee9492f386b764f8c5c
henhen724 <hshunt@uchicago.edu>
2020-06-18 02:57:17 -0400

    PARTICLES!

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
 import Header from '../components/header';
+import Particles from 'react-particles-js';
+import IntroEffectParams from '../public/particle-effects/intro.json';
 
 const Intro = () => {
-    return <div className="container bg-info text-center border mt-4 pb-2">
-        <Header title="Vulspa" />
-        This is a companion website to the Vulspa discord bot.  To see details on the bot itself you can head to its <a href="https://github.com/henhen724/Vulspa" className="text-dark bg-light">GitHub page</a>, or look at <a href="https://github.com/henhen724/Vulspa" className="text-dark bg-light">the GitHub Page for this cite.</a>  Both are currently under construction, so don't expect much.  If you'd like to get started anyway, you can <a href="/accounts/signup" className="text-dark bg-light">create a vuslpa account right now.</a> If your interested in contributing code or beta testing, message @Vulspa on twitter or join the <a href="https://discord.gg/g4fcbJu" className="text-dark bg-light">Vuslpa bot discord server.</a></div>
+
+    return (<><Header title="Vulspa" />
+        <div className="container bg-info text-center border mt-4 pb-2">
+            This is a companion website to the Vulspa discord bot.  To see details on the bot itself you can head to its <a href="https://github.com/henhen724/Vulspa">GitHub page</a>, or look at <a href="https://github.com/henhen724/Vulspa">the GitHub Page for this site.</a>  Both are currently under construction, so don't expect much.  If you'd like to get started anyway, you can <a href="/accounts/signup">create a vuslpa account right now.</a> If your interested in contributing code or beta testing, message @Vulspa on twitter or join the <a href="https://discord.gg/g4fcbJu">Vuslpa bot discord server.</a>
+        </div>
+        <Particles height="100vh" width="100vw" className="intro-particles" params={IntroEffectParams} />
+    </>)
 }
 
 export default Intro;
